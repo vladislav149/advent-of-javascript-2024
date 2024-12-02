c27e082f3a6358df1ba51e2a4b4fb345af892dd0
Vladislav <pocc19722@gmail.com>
2024-12-02 19:11:57 +0300

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/advent-of-javascript-2024',
+      path: '/',
       name: 'home',
       component: HomeView,
     },
