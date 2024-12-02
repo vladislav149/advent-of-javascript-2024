424e472b0a1d97f621f75d48f1eb042c952cdd69
Vladislav <pocc19722@gmail.com>
2024-12-02 19:10:16 +0300

    added deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/advent-of-javascript-2024',
       name: 'home',
       component: HomeView,
     },
